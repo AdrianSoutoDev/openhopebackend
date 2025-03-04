61f3dca78fdd93908cfa0723da32b090cafeb6cb
Adrian Souto <adrian.souto@udc.es>
2025-03-04 13:46:43 +0100

    Add configuration to Spring Security.

--- src/main/java/es/udc/OpenHope/config/SecurityConfig.java
+++ src/main/java/es/udc/OpenHope/config/SecurityConfig.java
@@ -1,9 +1,7 @@
 package es.udc.OpenHope.config;
 
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
