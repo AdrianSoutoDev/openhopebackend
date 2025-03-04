c4b0c073439cbdd780d35a4af3ac2b85297f425f
Adrian Souto <adrian.souto@udc.es>
2025-03-04 12:56:28 +0100

    First commit

---
+++ src/main/java/es/udc/OpenHope/OpenHopeApplication.java
@@ -0,0 +1,12 @@
+package es.udc.OpenHope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenHopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenHopeApplication.class, args);
+	}
+}
---
+++ src/test/java/es/udc/OpenHope/OpenHopeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.udc.OpenHope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenHopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
