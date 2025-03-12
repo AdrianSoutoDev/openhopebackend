2cf47a2276124b052ba006e1c8d2a51682ee1161
Adrian Souto <adrian.souto@udc.es>
2025-03-12 18:31:48 +0100

    Add validation to UserParams and create User Controller file.

--- src/main/java/es/udc/OpenHope/dto/UserParamsDto.java
+++ src/main/java/es/udc/OpenHope/dto/UserParamsDto.java
@@ -1,6 +1,17 @@
 package es.udc.OpenHope.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public class UserParamsDto {
+    @NotNull
+    @NotEmpty
+    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",flags = Pattern.Flag.CASE_INSENSITIVE)
     private String email;
+
+    @NotNull
+    @NotEmpty
     private String password;
 }
