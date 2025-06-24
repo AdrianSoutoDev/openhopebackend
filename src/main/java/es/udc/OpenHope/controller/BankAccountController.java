ece80acc96977d60b5ec2c56d760fa59bf37445a
Adrian Souto <adrian.souto@udc.es>
2025-06-25 01:20:13 +0200

    Add controller layer test for get donations and bank accounts.

--- src/main/java/es/udc/OpenHope/controller/BankAccountController.java
+++ src/main/java/es/udc/OpenHope/controller/BankAccountController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/bankAccounts")
+@RequestMapping("/api/bank-accounts")
 @Validated
 public class BankAccountController {
 
